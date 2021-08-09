779df597d06818227d2206dd3700df2e75cedd4a
Thifany Nicastro <thifanynicastro@hotmail.com>
2021-08-09 13:06:41 -0300

    posts

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,31 +1,77 @@
 import { Injectable } from '@nestjs/common';
-import { Post } from '@prisma/client';
+import { Post, Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
 export class PostsService {
+  constructor(private prisma: PrismaService) {}
+
   async findOne(id: number): Promise<Post | null> {
-    return undefined;
+    return this.prisma.post.findUnique({
+      where: { id },
+      include: {
+        author: true,
+      },
+    });
   }
 
   async findAll(): Promise<Post[]> {
-    return undefined;
+    return this.prisma.post.findMany();
   }
 
   async getPublishedPosts(): Promise<Post[]> {
-    return undefined;
+    return this.prisma.post.findMany({
+      where: {
+        published: true,
+      },
+      include: {
+        author: true,
+      },
+    });
   }
 
   async create(dto: CreatePostDto): Promise<Post> {
-    return undefined;
+    const { authorEmail } = dto;
+
+    delete dto.authorEmail;
+
+    const data: Prisma.PostCreateInput = {
+      ...dto,
+      author: {
+        connect: {
+          email: authorEmail,
+        },
+      },
+    };
+
+    return this.prisma.post.create({
+      data,
+      include: {
+        author: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
   }
 
   async update(id: number, dto: UpdatePostDto): Promise<Post> {
-    return undefined;
+    const data: Prisma.PostUpdateInput = {
+      ...dto,
+    };
+
+    return this.prisma.post.update({
+      data,
+      where: { id },
+    });
   }
 
   async remove(id: number): Promise<Post> {
-    return undefined;
+    return this.prisma.post.delete({
+      where: { id },
+    });
   }
 }
